bb1760e75b4e0d7830e327b30a0db7328afe6009
kendall <kendallcoleman@Kendalls-MacBook-Air.local>
2022-11-17 16:08:20 -0500

    added weather api

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
 var cityInput = document.querySelector(".input")
 var searchBtn = document.querySelector(".btn")
+
+
+
+
+
+
+function getWeatherApi() {
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=41.881832&lon=-87.623177&appid=edde692e58d867434cd1d621048e49c2&units=imperial"
+}
