5bab0a55a517ca6952cb71620a7410f67883b4d6
kendall <kendallcoleman@Kendalls-MacBook-Air.local>
2022-11-21 16:17:12 -0500

    fixing apis

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
 var cityInput = document.querySelector(".input")
 var searchBtn = document.querySelector(".btn")
+var descri = document.querySelector(".description")
+var temp = document.querySelector(".temp")
+var wind = document.querySelector(".wind")
+var humidity = document.querySelector(".humditiy")
+var visibility = document.querySelector(".visibilty")
 
-function citySearch() {
-    
-}
 
+searchBtn.addEventListener('click', function(){
+    var citySearch = {
+        function () {
+            fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityInput.value+"&limit=7&appid=edde692e58d867434cd1d621048e49c2"
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+        }
+    }
+})
 
-
-
-function getWeatherApi() {
-    var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=city&limit=7&appid=edde692e58d867434cd1d621048e49c2"
-}
